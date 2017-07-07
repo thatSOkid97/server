ddf11f088b80c1a646b6331ff6f181dd13aedbf7
sualko <klaus@jsxc.org>
2017-07-07 08:33:31 +0200

    add occ app:install command
    Signed-off-by: Klaus Herberth <klaus@jsxc.org>

---
+++ core/Command/App/Install.php
@@ -0,0 +1,106 @@
+<?php
+/**
+ * @copyright Copyright (c) 2016, ownCloud, Inc.
+ *
+ * @author Klaus Herberth <klaus@jsxc.org>
+ *
+ * @license AGPL-3.0
+ *
+ * This code is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License, version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License, version 3,
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ *
+ */
+
+namespace OC\Core\Command\App;
+
+use OC\Installer;
+use Stecman\Component\Symfony\Console\BashCompletion\Completion\CompletionAwareInterface;
+use Stecman\Component\Symfony\Console\BashCompletion\CompletionContext;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class Install extends Command implements CompletionAwareInterface {
+
+	public function __construct() {
+		parent::__construct();
+	}
+
+	protected function configure() {
+		$this
+			->setName('app:install')
+			->setDescription('install an app')
+			->addArgument(
+				'app-id',
+				InputArgument::REQUIRED,
+				'install the specified app'
+			)
+		;
+	}
+
+	protected function execute(InputInterface $input, OutputInterface $output) {
+		$appId = $input->getArgument('app-id');
+
+		if (\OC_App::getAppPath($appId)) {
+			$output->writeln($appId . ' already installed');
+			return 1;
+		}
+
+		try {
+			$installer = new Installer(
+				\OC::$server->getAppFetcher(),
+				\OC::$server->getHTTPClientService(),
+				\OC::$server->getTempManager(),
+				\OC::$server->getLogger(),
+				\OC::$server->getConfig()
+			);
+			$installer->downloadApp($appId);
+			$result = $installer->installApp($appId);
+		} catch(Exception $e) {
+			$output->writeln('Error: ' . $ex->getMessage());
+			return 1;
+		}
+
+		if($result === false) {
+			$output->writeln($appId . ' couldn\'t be installed');
+			return 1;
+		}
+
+		$output->writeln($appId . ' installed');
+
+		return 0;
+	}
+
+	/**
+	 * @param string $optionName
+	 * @param CompletionContext $context
+	 * @return string[]
+	 */
+	public function completeOptionValues($optionName, CompletionContext $context) {
+		return [];
+	}
+
+	/**
+	 * @param string $argumentName
+	 * @param CompletionContext $context
+	 * @return string[]
+	 */
+	public function completeArgumentValues($argumentName, CompletionContext $context) {
+		if ($argumentName === 'app-id') {
+			$allApps = \OC_App::getAllApps();
+			return array_diff($allApps, \OC_App::getEnabledApps(true, true));
+		}
+		return [];
+	}
+}

ebb137b7f2c3e8928c68e727c3518c27fc1bc7af
sualko <klaus@jsxc.org>
2017-07-07 13:31:11 +0200

    remove unused use statements

--- core/Command/App/Install.php
+++ core/Command/App/Install.php
@@ -23,11 +23,9 @@
 namespace OC\Core\Command\App;
 
 use OC\Installer;
-use Stecman\Component\Symfony\Console\BashCompletion\CompletionContext;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class Install extends Command {
